803b197abe8b30a1a64d9b9d79e11ad4a3a5af8d
sanyaswb <sanyaswb@gmail.com>
2022-11-24 12:54:17 +0200

    completed 34_function

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+function addition(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
 function calc(a, sign, b) {
 	switch (sign) {
 		case '+':
-			return a + b;
+			return addition(a, b);
 		case '-':
-			return a - b;
+			return subtract(a, b);
 		case '*':
-			return a * b;
+			return multiply(a, b);
 		case '/':
-			return a / b;
+			return divide(a, b);
 	}
 }
 
